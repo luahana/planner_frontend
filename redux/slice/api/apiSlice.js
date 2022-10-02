819dd8118880da1a1fab99e301dea0015ffb47d4
Jinsea Park <truepark0@gmail.com>
2022-10-01 21:54:17 -0400

    changed url

--- redux/slice/api/apiSlice.js
+++ redux/slice/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../authSlice'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'planner-7rl5-2wnj1u6cs-luahana.vercel.app',
+  baseUrl: 'planner-7epn.vercel.app',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token
