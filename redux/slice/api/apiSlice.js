1899ab82f7f770182740eba1ebc22b50d2ed181f
Jinsea Park <truepark0@gmail.com>
2022-10-01 22:01:08 -0400

    changed url with https

--- redux/slice/api/apiSlice.js
+++ redux/slice/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../authSlice'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://planner-7epn.vercel.app',
+  baseUrl: 'https://planner-7epn.vercel.app',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token
