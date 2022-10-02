ef37762571e364ccf832cd7e836924fe28dea77c
Jinsea Park <truepark0@gmail.com>
2022-10-01 20:13:54 -0400

    changed api address

--- redux/slice/api/apiSlice.js
+++ redux/slice/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../authSlice'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:8080/api',
+  baseUrl: 'https://simple-todo-planner-api.onrender.com',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token
