8ebdf9a8bdd1ebbe6a7eb362491810d857f60a0c
Jinsea Park <truepark0@gmail.com>
2022-10-08 22:15:28 -0400

    test ios browser refresh issue

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -4,6 +4,8 @@ import jwtDecode from 'jwt-decode'
 
 const useAuth = () => {
   const token = useSelector(selectCurrentToken)
+  console.log('token')
+  console.log(token)
   if (token) {
     const decoded = jwtDecode(token)
     const { _id, email, name, isAdmin } = decoded
